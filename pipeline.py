187ab23aa9ab3b3737486a7e703ef1b60e22c02a
ana <anampfrias@gmail.com>
2024-01-24 14:51:52 +0000

    import library

--- pipeline.py
+++ pipeline.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import psycopg2
 
 url = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/yellow_tripdata_2021-01.csv.gz'
 
